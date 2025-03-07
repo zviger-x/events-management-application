a4178a781fbbaa17880b704f87df5529e77dac00
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-03-07 14:53:12 +0300

    Added user db context

---
+++ Users Microservice/DataAccess/Contexts/UserDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Contexts
+{
+    internal class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserNotification> UserNotifications { get; set; }
+        public DbSet<UserTransaction> UserTransactions { get; set; }
+    }
+}

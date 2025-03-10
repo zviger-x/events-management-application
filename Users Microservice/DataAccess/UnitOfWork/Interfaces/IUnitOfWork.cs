5224439f2232ab00a2772b876c9c8abbb9397631
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-03-10 11:58:18 +0300

    Added UnitOfWork

---
+++ Users Microservice/DataAccess/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Repositories.Interfaces;
+
+namespace DataAccess.UnitOfWork.Interfaces
+{
+    internal interface IUnitOfWork : IDisposable
+    {
+        IUserRepository UserRepository { get; }
+        IUserNotificationRepository UserNotificationRepository { get; }
+        IUserTransactionRepository UserTransactionRepository { get; }
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ Users Microservice/DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Contexts;
+using DataAccess.Repositories.Interfaces;
+using DataAccess.UnitOfWork.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess.UnitOfWork
+{
+    internal class UnitOfWOrk : IUnitOfWork
+    {
+        private readonly UserDbContext _context;
+        private readonly IServiceProvider _serviceProvider;
+
+        private IUserRepository _userRepository;
+        private IUserNotificationRepository _userNotificationRepository;
+        private IUserTransactionRepository _userTransactionRepository;
+
+        public UnitOfWOrk(UserDbContext context, IServiceProvider serviceProvider)
+        {
+            _context = context;
+            _serviceProvider = serviceProvider;
+        }
+
+        public IUserRepository UserRepository
+        {
+            get => _userRepository ??= _serviceProvider.GetRequiredService<IUserRepository>();
+        }
+
+        public IUserNotificationRepository UserNotificationRepository
+        {
+            get => _userNotificationRepository ??= _serviceProvider.GetRequiredService<IUserNotificationRepository>();
+        }
+
+        public IUserTransactionRepository UserTransactionRepository
+        {
+            get => _userTransactionRepository ??= _serviceProvider.GetRequiredService<IUserTransactionRepository>();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}

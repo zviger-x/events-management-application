3e106ec331a7760ac35efc50908ccfecbded1be0
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-03-12 19:42:03 +0300

    Added test controller to check migration

---
+++ UsersMicroservice/UsersAPI/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogic.Models;
+using BusinessLogic.Services.Interfaces;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public Response<IQueryable<User>> GetAll()
+        {
+            return _userService.GetAll();
+        }
+    }
+}

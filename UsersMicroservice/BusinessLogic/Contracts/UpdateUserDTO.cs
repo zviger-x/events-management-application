2301351e1b13a214a16c37c119f9a08a6000d3c8
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-03-17 23:33:51 +0300

    Added contracts

---
+++ UsersMicroservice/BusinessLogic/Contracts/UpdateUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BusinessLogic.Contracts
+{
+    public class UpdateUserDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}

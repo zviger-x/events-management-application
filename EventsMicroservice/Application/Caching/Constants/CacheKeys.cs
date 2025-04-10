1750338fec31688aa8396f4b6e743aadedf92ae3
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-04-10 15:38:20 +0300

    Added cache keys

---
+++ EventsMicroservice/Application/Caching/Constants/CacheKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Caching.Constants
+{
+    public static class CacheKeys
+    {
+        public static string EventById(Guid id) => $"events:{id}";
+        public static string PagedEvents(int pageNumber, int pageSize) => $"events:paged:{pageNumber}:{pageSize}";
+    }
+}

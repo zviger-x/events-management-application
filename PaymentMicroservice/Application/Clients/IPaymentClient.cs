498123d3004b4c98ab9767079267ca9101a9cdd4
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-04-24 22:54:31 +0300

    Added client interfaces

---
+++ PaymentMicroservice/Application/Clients/IPaymentClient.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Clients
+{
+    /// <summary>
+    /// Interface for handling payment operations with external payment providers.
+    /// </summary>
+    public interface IPaymentClient
+    {
+        /// <summary>
+        /// Processes a payment using the provided token and amount.
+        /// </summary>
+        /// <param name="token">The payment token generated from user card details.</param>
+        /// <param name="amount">The amount to be charged for the payment.</param>
+        /// <returns>True if payment was successful, false otherwise.</returns>
+        Task<bool> ProcessPaymentAsync(string token, float amount);
+    }
+}
---
+++ PaymentMicroservice/Application/Clients/IUserClient.cs
@@ -0,0 +1,17 @@
+﻿using Application.Contracts;
+
+namespace Application.Clients
+{
+    /// <summary>
+    /// Interface for interacting with the User microservice to manage user-related data.
+    /// </summary>
+    public interface IUserClient
+    {
+        /// <summary>
+        /// Creates a new user transaction in the system.
+        /// </summary>
+        /// <param name="transaction">The transaction data to be saved.</param>
+        /// <returns>True if transaction was successfully created, false otherwise.</returns>
+        Task<bool> CreateTransactionAsync(CreateUserTransactionDto transaction);
+    }
+}

abf6f3c3e42da36583d0614666b357dbb878a557
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-04-24 22:55:23 +0300

    Added payment handler

---
+++ PaymentMicroservice/Application/MediatR/Handlers/ProcessPaymentCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Application.Clients;
+using Application.Contracts;
+using Application.MediatR.Commands;
+using AutoMapper;
+using MediatR;
+
+namespace Application.MediatR.Handlers
+{
+    public class ProcessPaymentCommandHandler : IRequestHandler<ProcessPaymentCommand, PaymentResultDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserClient _userClient;
+        private readonly IPaymentClient _paymentClient;
+
+        public ProcessPaymentCommandHandler(IMapper mapper, IUserClient userClient, IPaymentClient paymentClient)
+        {
+            _mapper = mapper;
+            _userClient = userClient;
+            _paymentClient = paymentClient;
+        }
+
+        public async Task<PaymentResultDto> Handle(ProcessPaymentCommand request, CancellationToken cancellationToken)
+        {
+            // Оплачиваем
+            var success = await _paymentClient.ProcessPaymentAsync(request.Token, request.Amount);
+            if (!success)
+                return new PaymentResultDto { Success = success };
+
+            var transaction = _mapper.Map<CreateUserTransactionDto>(request);
+
+            // Сохраняем транзакцию в истории
+            await _userClient.CreateTransactionAsync(transaction);
+
+            // Возвращаем успешный статус
+            return new PaymentResultDto { Success = success };
+        }
+    }
+}

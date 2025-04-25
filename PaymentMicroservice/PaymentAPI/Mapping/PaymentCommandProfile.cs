8841ac3998ebb9fd62f9a767992ecb6b09385dc5
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-04-25 11:33:24 +0300

    Added payment service

---
+++ PaymentMicroservice/PaymentAPI/Mapping/PaymentCommandProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.MediatR.Commands;
+using AutoMapper;
+using PaymentAPI.Services.Grpc.Payment;
+
+namespace PaymentAPI.Mapping
+{
+    public class PaymentCommandProfile : Profile
+    {
+        protected PaymentCommandProfile()
+        {
+            CreateMap<ProcessPaymentRequest, ProcessPaymentCommand>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => Guid.Parse(src.UserId)))
+                .ForMember(dest => dest.EventId, opt => opt.MapFrom(src => Guid.Parse(src.EventId)));
+        }
+    }
+}
---
+++ PaymentMicroservice/PaymentAPI/Services/PaymentService.cs
@@ -0,0 +1,30 @@
+﻿using Application.MediatR.Commands;
+using AutoMapper;
+using Grpc.Core;
+using MediatR;
+using PaymentAPI.Services.Grpc.Payment;
+using GrpcPaymentService = PaymentAPI.Services.Grpc.Payment.PaymentService;
+
+namespace PaymentAPI.Services
+{
+    public class PaymentService : GrpcPaymentService.PaymentServiceBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public PaymentService(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public override async Task<ProcessPaymentResponse> ProcessPayment(ProcessPaymentRequest request, ServerCallContext context)
+        {
+            var command = _mapper.Map<ProcessPaymentCommand>(request);
+
+            var result = await _mediator.Send(command, context.CancellationToken);
+
+            return new ProcessPaymentResponse { Success = result.Success };
+        }
+    }
+}

fb11eb692b688848479114f56a9f97b313579a6a
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-05-08 22:34:49 +0300

    Fixed notification resender

--- NotificationsMicroservice/Application/Services/Background/FailedNotificationResender.cs
+++ NotificationsMicroservice/Application/Services/Background/FailedNotificationResender.cs
@@ -3,81 +3,80 @@ using Application.Contracts;
 using Application.MediatR.Commands;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Shared.Caching.Services.Interfaces;
+using Shared.Services.Background;
 
 namespace Application.Services.Background
 {
-    public class FailedNotificationResender : BackgroundService
+    public class FailedNotificationResender : AdvancedBackgroundService
     {
-        private const int MaxDegreeOfParallelism = 5;
-
         private readonly IServiceScopeFactory _scopeFactory;
         private readonly ILogger<FailedNotificationResender> _logger;
         private readonly TimeSpan _interval = TimeSpan.FromSeconds(30);
 
-        public FailedNotificationResender(IServiceScopeFactory serviceScopeFactory, ILogger<FailedNotificationResender> logger)
+        public FailedNotificationResender(IServiceScopeFactory scopeFactory, ILogger<FailedNotificationResender> logger)
+            : base(logger)
         {
-            _scopeFactory = serviceScopeFactory;
+            _scopeFactory = scopeFactory;
             _logger = logger;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        protected override Task InitializeAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Notification retry background service started.");
 
-            while (!cancellationToken.IsCancellationRequested)
+            return Task.CompletedTask;
+        }
+
+        protected async override Task ExecuteIterationAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(_interval, cancellationToken);
+
+            try
             {
-                await Task.Delay(_interval, cancellationToken);
+                using var scope = _scopeFactory.CreateScope();
 
-                try
+                var cacheService = scope.ServiceProvider.GetRequiredService<IRedisCacheService>();
+                var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+                // Для начала получаем ожидающие уведомления
+                var cacheSetKey = CacheKeys.FailedNotificationsSet;
+
+                var failedNotifications = await cacheService.GetSetMembersAsync<NotificationDto>(cacheSetKey, cancellationToken);
+
+                // Если их нет, пропускаем текущую итерацию
+                if (!failedNotifications.Any())
+                    return;
+
+                // Если они есть, то в через Parallel.ForEachAsync пытаемся их переотправить.
+                _logger.LogInformation("Found {Count} failed notifications to retry", failedNotifications.Count());
+
+                foreach (var notification in failedNotifications)
                 {
-                    using var scope = _scopeFactory.CreateScope();
-
-                    var cacheService = scope.ServiceProvider.GetRequiredService<IRedisCacheService>();
-                    var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-
-                    // Для начала получаем ожидающие уведомления
-                    var cacheSetKey = CacheKeys.FailedNotificationsSet;
-
-                    var failedNotifications = await cacheService.GetSetMembersAsync<NotificationDto>(cacheSetKey, cancellationToken);
-
-                    // Если их нет, пропускаем текущую итерацию
-                    if (!failedNotifications.Any())
-                        continue;
-
-                    // Если они есть, то в через Parallel.ForEachAsync пытаемся их переотправить.
-                    _logger.LogInformation("Found {Count} failed notifications to retry", failedNotifications.Count());
-
-                    var parallelOptions = new ParallelOptions
+                    try
                     {
-                        MaxDegreeOfParallelism = MaxDegreeOfParallelism,
-                        CancellationToken = cancellationToken
-                    };
+                        var command = new SendNotificationCommand { Notification = notification };
 
-                    await Parallel.ForEachAsync(failedNotifications, parallelOptions,
-                        async (notification, token) =>
-                        {
-                            try
-                            {
-                                var command = new SendNotificationCommand { Notification = notification };
-
-                                await mediator.Send(command, token);
-                            }
-                            catch (Exception ex)
-                            {
-                                _logger.LogError(ex, "Failed to resend notification for user {UserId}", notification.UserId);
-                            }
-                        });
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Error while retrying notifications");
+                        await mediator.Send(command, cancellationToken);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Failed to resend notification for user {UserId}", notification.UserId);
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while retrying notifications");
+            }
+        }
 
+        protected override Task CleanupAsync(CancellationToken cancellationToken)
+        {
             _logger.LogInformation("Notification retry background service stopped.");
+
+            return Task.CompletedTask;
         }
     }
 }

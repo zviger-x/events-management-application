5ba4f6724093636915c680cbeaab43bc85a03e14
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-05-08 22:36:14 +0300

    Added kafka message handler

---
+++ NotificationsMicroservice/Infrastructure/Kafka/MessageHandlers/Interfaces/Common/IKafkaMessageHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Kafka.MessageHandlers.Interfaces.Common
+{
+    public interface IKafkaMessageHandler
+    {
+        string Topic { get; init; }
+
+        Task HandleMessage(string message, CancellationToken cancellationToken);
+    }
+}
---
+++ Shared/Shared/Configuration/KafkaServerConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Configuration
+{
+    public class KafkaServerConfig
+    {
+        public string BootstrapServers { get; set; }
+        public string GroupId { get; set; }
+        public Dictionary<string, string> Topics { get; set; }
+
+        public string EventStub => Topics["EventStub"];
+    }
+}

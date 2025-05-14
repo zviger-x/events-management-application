f125771be86f4493b7eae61e122e8f237503fe23
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-05-14 11:17:35 +0300

    Split profiles

--- UsersMicroservice/BusinessLogic/Mapping/UserDtoProfile.cs
+++ UsersMicroservice/BusinessLogic/Mapping/UserDtoProfile.cs
@@ -4,17 +4,12 @@ using DataAccess.Entities;
 
 namespace BusinessLogic.Mapping
 {
-    public class UserProfile : Profile
+    public class UserDtoProfile : Profile
     {
-        public UserProfile()
+        public UserDtoProfile()
         {
-            CreateMap<UpdateUserDto, User>();
-            CreateMap<User, UpdateUserDto>();
-
-            CreateMap<RegisterDto, User>();
-            CreateMap<User, RegisterDto>();
-
             CreateMap<UserDto, User>();
+
             CreateMap<User, UserDto>()
                 .ForMember(dest => dest.Notifications, opt => opt.Condition(u => u.Notifications != null))
                 .ForMember(dest => dest.Transactions, opt => opt.Condition(u => u.Transactions != null));
---
+++ UsersMicroservice/BusinessLogic/Mapping/UserUpdateProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BusinessLogic.Contracts;
+using DataAccess.Entities;
+
+namespace BusinessLogic.Mapping
+{
+    public class UserUpdateProfile : Profile
+    {
+        public UserUpdateProfile()
+        {
+            CreateMap<UpdateUserDto, User>();
+            CreateMap<User, UpdateUserDto>();
+        }
+    }
+}
